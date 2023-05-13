bb11714442a97bd061a73d4e01373cb2729dcdb2
Michel Koekkoek <michelkoekkoek@gmail.com>
2023-05-13 20:17:08 +0200

    Add first H8 exercises

---
+++ H8/favorite_book.py
@@ -0,0 +1,4 @@
+def favorite_book(title):
+    print(f"One of my favorite books is {title}")
+
+favorite_book("Seven Habbits of Healy Effectivgit e People")
---
+++ H8/message.py
@@ -0,0 +1,4 @@
+def display_message():
+    print("I'm learning how to code functions in Python.")
+
+display_message()
