28e331a991bc8ce6099803ee17e487d9c7d85746
Michel Koekkoek <michelkoekkoek@gmail.com>
2023-05-16 20:50:40 +0200

    Changes to H7 deli.py

--- H7/deli.py
+++ H7/deli.py
@@ -5,14 +5,14 @@ while sandwich_orders:
     # Pop sandwich from order and store it in temp value
     sandwich = sandwich_orders.pop()
 
-    #Print a 'sandwich ready' order
+    # Print a 'sandwich ready' order
     print(f"I made your {sandwich} sandwich.")
-    
-    #Add sandwich to new list
+
+    # Add sandwich to new list
     finished_sandwiches.append(sandwich)
 
 print("\nThat's it. I've made the following sandwiches:")
 
 for sandwich in finished_sandwiches:
-    #print(f"* {sandwich}")
+    # print(f"* {sandwich}")
     print(f"* {sandwich.title()} sandwich")
